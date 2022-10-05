5be6945e7b27a94fd841f6a10e8b47e53d934088
okb-777 <bartekkszke2@gmail.com>
2022-10-05 12:41:56 +0200

    poprawka

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php
-    $con = new mysqli("127.0.0.1", "root", "", "memes");
+    $db = new mysqli("127.0.0.1", "root", "", "memes");
 
     $page = 1;
     $limit = 10;
@@ -14,32 +14,33 @@
         <title>Memy</title>
     </head>
     <body>
-             <?php 
-                    if ($wynik = $con->query($q)){
-                    
-                        while($row=$wynik->fetch_array()){
-                            //print_r($row);
-                            echo '
-                                <div class="date_title">
-                                    <div class="title"><p>'.$row["title"].'</p></div>
-                                    <div class="date"><p>'.$row["created_ad"].'</p></div>
-                                </div>
-                                <div class="where_meme"></div>
-                                <div class="meme">
-                                <div>
-                                    <img class="meme_1" src="'.$row["file"].'" alt="'.$row["title"].'">
-                                </div>
-                                </div>
-                                    <div class="where_meme">Musze dać gdzieś like XD'.$row["like"].'</div>
-                                </div>
-                                ';
-                        }
-                    }
-            ?>
         <div class="pages">
             <h2>Tu są strony</h2>
-            
-            <button> <!-- </button> 1 <button> --> </button>
+            <a src="commands.php">
+                <button class="button"> joł </button>
+            </a>
         </div>
+        
+        <?php 
+            if ($wynik = $db->query($q)){
+                    
+                while($row=$wynik->fetch_array()){
+                //print_r($row);
+                echo '
+                    <div class="date_title">
+                        <div class="title"><p>'.$row["title"].'</p></div>
+                        <div class="date"><p>'.$row["created_ad"].'</p></div>
+                    </div>
+                    <div class="where_meme"></div>
+                    <div class="meme">
+                        <div>
+                            <img class="meme_1" src="'.$row["file"].'" alt="'.$row["title"].'">
+                        </div>
+                    </div>
+                    <div class="where_meme">Musze dać gdzieś like XD '.$row["like"].'</div>
+                ';
+                }
+            }
+        ?>
     </body>
 </html>
